fdee5a7dbe6a7eb34a054d7ad5ade69a8396e740
Mitya <158564922+MityaMikhailov@users.noreply.github.com>
2024-09-02 16:47:33 +0500

    Create entity Task and Task+CoreDataProperties.swift

---
+++ ToDo_List/Models/Task+CoreDataProperties.swift
@@ -0,0 +1,33 @@
+//
+//  Task+CoreDataProperties.swift
+//  ToDo_List
+//
+//  Created by Dmitriy Mikhailov on 02.09.2024.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
+
+extension Task {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Task> {
+        return NSFetchRequest<Task>(entityName: "Task")
+    }
+
+    @NSManaged public var taskId: Int16
+    @NSManaged public var dateCreate: Date?
+    @NSManaged public var executionStatus: Bool
+    @NSManaged public var taskDescription: String?
+    @NSManaged public var taskName: String?
+
+}
+
+extension Task : Identifiable {
+
+}
